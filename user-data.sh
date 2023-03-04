ac07f32802c5921edde1f4bdd2c2e0938ce7380a
JFMajer <jakubmajer@live.com>
2023-03-04 17:10:15 +0100

    remove db address and port

--- user-data.sh
+++ user-data.sh
@@ -10,8 +10,6 @@ PUBLIC_HOSTNAME="$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 
 cat > index.html <<EOF
 <h1>${server_text}</h1>
-<p>DB address: ${db_address}</p>
-<p>DB port: ${db_port}</p>
 <hr>
 <p>Loadbalances to EC2 instance of:
 <ul>

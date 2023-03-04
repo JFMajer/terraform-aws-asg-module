f5191da6aa1aa2bc9908dcb8f3612a1daab41524
JFMajer <jakubmajer@live.com>
2023-03-04 12:19:32 +0100

    added http output

--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,10 @@ output http_url {
   value = "http://${aws_lb.asg_lb.dns_name}"
 }
 
+output https_url {
+  value = "https://${aws_lb.asg_lb.dns_name}"
+}
+
 output "alb_zone_id" {
   value = aws_lb.asg_lb.zone_id
   description = "Zone ID of the load balancer"

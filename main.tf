2e78e156c6c01d584e48aae9b5014c138a3be060
JFMajer <jakubmajer@live.com>
2023-03-07 19:33:04 +0100

    fixing launch template

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_launch_template" "asg_lt" {
     }
   }
 
-  user_data = filebase64(templatefile("${path.module}/user-data.sh", {
+  user_data = filebase64(templatefile("user-data.sh", {
     server_text = var.server_text,
     server_port = var.server_port
   }))
